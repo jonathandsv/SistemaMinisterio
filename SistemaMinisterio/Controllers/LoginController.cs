1813b7054bfec7b0a56614dd6bff5d1307f68fc6
jonathan.vieira <jonathan.vieira@cebraspe.org.br>
2019-05-31 17:53:52 -0300

    Add project files.

---
+++ SistemaMinisterio/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaMinisterio.Models;
+
+namespace SistemaMinisterio.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario user)
+        {
+            
+        }
+    }
+}
---
+++ SistemaMinisterio/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaMinisterio.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int IdPerfilMembro { get; set; }
+        public int IdPerfilUsuario { get; set; }
+        public int Senha { get; set; }
+        public string Email { get; set; }
+        public string Sexo { get; set; }
+        public int Idade { get; set; }
+    }
+}
